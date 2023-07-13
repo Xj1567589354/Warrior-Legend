2a2c21b9d6822ce25df94bc5e71df0f4fff84e46
XJ <1248119887@qq.com>
2023-07-13 13:38:51 +0800

    新增野猪死亡和受伤逻辑

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -13,12 +15,20 @@ public class Enemy : MonoBehaviour
     public float normalSpeed;   // 移动速度
     public float chaseSpeed;    // 追击速度
     public float currentSpeed;  // 当前速度
+    public float hurtForce;     // 受伤力
     public Vector3 faceDir;     // 面朝方向
+    public Vector2 hurtDir;     // 受伤方向
+
+    private Transform attacker;  // 攻击者
 
     [Header("计时器")]
     public float waitTime;      // 等待时间
     public float waitTimeCounter;   // 计时器
     public bool wait;       // 等待状态
+
+    [Header("状态")]
+    public bool isHurt;     // 受伤
+    public bool isDead;     // 死亡
 
     private void Awake()
     {
@@ -56,7 +66,8 @@ public class Enemy : MonoBehaviour
 
     public virtual void Move()
     {
-        rb.velocity = new Vector2(faceDir.x * currentSpeed * Time.fixedDeltaTime, rb.velocity.y);
+        if(!isHurt && !isDead)
+            rb.velocity = new Vector2(faceDir.x * currentSpeed * Time.fixedDeltaTime, rb.velocity.y);
     }
 
     /// <summary>
@@ -74,7 +85,52 @@ public class Enemy : MonoBehaviour
                 waitTimeCounter = waitTime;     // 重置计时器
                 transform.localScale = new Vector3(faceDir.x, 1, 1);       // 转身
             }
-
         }
+    }
+
+     public void OnTakeDamage(Transform attackerTrans)
+    {
+        // 玩家
+        attacker = attackerTrans;    
+        // 转身
+        if (attacker.position.x - transform.position.x > 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        if (attacker.position.x - transform.position.x < 0)
+            transform.localScale = new Vector3(1, 1, 1);
+
+        // 受伤击退
+        isHurt = true;
+        animator.SetTrigger("hurt");
+        hurtDir = new Vector2(transform.position.x - attacker.position.x, 0).normalized;
+
+        StartCoroutine(OnHurt(hurtDir));
+    }
+    
+    /// <summary>
+    /// 协程方法
+    /// </summary>
+    /// <param name="_hurtDir">受伤方向</param>
+    /// <returns></returns>
+    private IEnumerator OnHurt(Vector2 _hurtDir)
+    {
+        rb.AddForce(_hurtDir * hurtForce, ForceMode2D.Impulse);
+        // 等待0.45s之后结束受伤状态
+        yield return new WaitForSeconds(0.5f);    
+        isHurt = false;
+    }
+
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    public void OnDie()
+    {
+        isDead = true;
+        animator.SetBool("dead", isDead);
+        gameObject.layer = 2;
+    }
+
+    public void OnDestroyAfterAnimation()
+    {
+        Destroy(this.gameObject);
     }
 }
